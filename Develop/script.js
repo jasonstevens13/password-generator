9d6cdf6d2ad85eeb5843ef86e484f67358d82b7d
jasonstevens13 <jasonstevens13@yahoo.com>
2020-03-14 09:49:15 -0400

    Add arrays of characters. Started generatePassword function.

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowerCaseChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCaseChar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var specialChar = ['!', '"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '>', '=', '?', '@', '[', ']', '^', '_', '`', '{', '}', '~'];
 
 // Write password to the #password input
 function writePassword() {
@@ -12,3 +15,23 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+function generatePassword() {
+  var charLength = prompt('How many characters would you like your password to be? Requirments: between 8 and 128 characters.', num1);
+  if (charLength >= 8 && charLength <= 128) {
+    alert('Thank You');
+    return;
+  } else {
+    alert('Sorry, that will not work. Please select a number between 8 and 128.');
+    generatePassword();
+  }
+}
+
+
+
+
+  // Then prompt whether using lower case.
+  // Then prompt whether using uppercase.
+  // Then prompt whether using special characters.
+}
