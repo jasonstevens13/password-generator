1be4d4b19946deb0ddddd0b3fe0c940fbaf51abd
jasonstevens13 <jasonstevens13@yahoo.com>
2020-03-17 23:11:12 -0400

    additional comments and read me

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
-# password-generator
+# Password-Generator
 password-generator
 
-# work-in-progress
+# Description
+This password generator application, does just that - generates a password. However, the important attribute is that the application generates a random password based on user input. These features include prompts and confirms to combine character types that the user would like included. 
+
+# Importance
+Creating a password at random is an important option for a user. This application exemplifies, in small scope, the simple use-case of the need for a unique password/passcode. This may best be suited for a one-time authentication for a user's access to a website for example; or to reset a forgotten password. One can imagine many uses for a similar application functionality.
+
+# Build
+The application was built by way of html, bootstrap styling therein, and Javascript functionality. The JS functionality exists in the browser.
+
+# How to Use
+1. The user must click the 'Generate Password' button to initiate the applications functionality.
+![app](./assets/img1.jpg)
+2. The use is then prompted with a question, "How many characters would you like your password to be? Requirement: between 8 and 128." 
+![charNumPrompt](./assets/img2.jpg)
+3. If the user enters an input that is not a numeral, or a number that is less than 8 or greater than 128, they are alerted with, "Your password length must be between 8 and 128 characters. Please try again."
+![alert](./assets/img3.jpg)
+4. Once a valide number is entered, the user is presented with the following confirm prompts that are answered as yes/no response: 'Do you want to use upper case characters?;' 'Do you want to use lower case characters?;' 'Do you want to use special characters?;' 'Do you want to use numbers in your password?'
+![confirmExample](./assets/img4.jpg)
+5. The application then traverses through a string of all character types chosen, and generates a random password that is the same number of characters as the user's initial input.
+![result](./assets/img5.jpg)
--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+// I created the following variables to take the strings of the different char types the user can select.
 var lowerCaseChar = 'abcdefghijklmnopqrstuvwxyz';
 var upperCaseChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var specialChar = '!"#$%&()*+-./:;<>=?@[]^_`{}~';
 var numberTypeChar = '0123456789';
-var allCharTypes = '';
+// The following variable 'tempPassword2' is initialized as an empty string, which will later take the chars from user selection of char types and random selection of specific chars. 
 var tempPassword2 = '';
-// var passwordCharLengthFinal = passwordCharLength;
+
 
 // Write password to the #password input
 function writePassword() {
@@ -19,10 +20,17 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+// The rest of the code will take place in the following 'generatePassword' function. From this we will ultimately return a string of chars that make up the password.
 function generatePassword() {
+    // Variable containing an empty string initialized. This will take chars later.
     var tempPassword = '';
     var tempPassword2 = '';
+
+    // The first prompt asks the user to select the number of chars they'd like in their password, and stores their number response in a variable named passwordCharLength. 
     var passwordCharLength = Number(prompt('How many characters would you like your password to be? Requirement: between 8 and 128.', ""));
+
+    // The following if statement checks to make sure user selects a number betwee 8 and 128 as instructed; 
+    // and if not they are alerted and have to begin the process anew. We would clear the function and start from the top.
     if (passwordCharLength >= 8 && passwordCharLength <= 128) {
     } else {
         alert('Your password length must be between 8 and 128 characters. Please try again.');
@@ -30,11 +38,16 @@ function generatePassword() {
         return;
     }
 
+    // We then ask the use with confirms (Ok or Cancel) , the following char types they'd like to include in their password.
+    // These answers, which are essentially true/false, are stored in their respective varable names.
     var passwordUpperCase = confirm('Do you want to use upper case characters?');
     var passwordLowerCase = confirm('Do you want to use lower case characters?');
     var passwordSpecialChars = confirm('Do you want to use special characters?');
     var passwordNumberChars = confirm('Do you want to use numbers in your password?');
 
+
+    // The folllowing (4) 'if' statement conditionals simlply take the sets of chars for each confirmed response,
+    // and concatenates the string to the 'tempPassword' variable string that started off as blank.
     if (passwordUpperCase) {
         tempPassword = tempPassword + upperCaseChar;
     }
@@ -51,19 +64,29 @@ function generatePassword() {
         tempPassword = tempPassword + numberTypeChar;
     }
 
-    // At least one of the char types must be chosen, so we have to create a block..
+
+    // At least one of the char types must be chosen, so we have to create a check. 
+    // If the user clicked cancel through every confirm prompt, they are alerted that they must choose one and to start over.
     if (passwordUpperCase === false && passwordLowerCase === false && passwordSpecialChars === false && passwordNumberChars === false) {
         alert('You must choose at least 1 character type. Please start over.');
         generatePassword();
         return;
     }
 
+    // Once all of the user's selections have been concatenated into one string named the 'tempPassword' variable,
+    // we must iterate through the string to extract random characters, at the same volume as the user's original number selection.
+    // The original number selection was store in the 'passwordCharLength' variable seen in the for loop below.
     for (var i = 0; i < passwordCharLength; i++) {
         var c = tempPassword.charAt(Math.floor(Math.random() * tempPassword.length));
+
+        // We then introduce 'tempPassword2' variable wich is the other string that was initialized as blank. 
+        // As we interate through the cocatenated string of chars, we grab each randomly selected char and add them together
+        // in the 'tempPAssword2' string variable.  
         tempPassword2 += c;
     }
-
+    // The final step in this function is to return the new password that's been created. Therefore this entire function,
+    // named 'generatePassword' returns the product that is 'tempPassword2'. This result is then passed back
+    // to the original function 'writePassword' which was waiting for the result. 
     return (tempPassword2);
 
 }
-
